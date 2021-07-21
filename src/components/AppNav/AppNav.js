104b98eb45cafcd43f7be21df6713833f96ea596
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-07-21 13:36:45 -0500

    Shawn Componenet III solution

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 
 class AppNav extends Component {
 
+  renderNavItems = () => {
+    return this.props.navItems.map((item, index) => { return ( <span>&nbsp;&nbsp; <a href="#" key={`nav-${index}`} onClick={ () => this.props.handleNavClick(item.value) }>{ item.label } </a>
+    &nbsp;&nbsp;| </span> ) } 
+    )
+  }
+
+
   render() {
     return (
       <nav>
+        { this.renderNavItems() }
       </nav>
     )
   }
 }
 
 export default AppNav;
+
+// href="" key={`nav-${index}`} onClick={ () => this.props.handleNavClick(item.value)}
