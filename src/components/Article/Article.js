b551add14dc77a00a0449655ab9e0b2d481bd1cc
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-07-21 13:09:07 -0500

    Shawn Component II solution

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -4,9 +4,15 @@ class Article extends Component {
   render() {  
     return (
       <div>
+        <h1> {this.props.title } </h1>
+        <p> {this.props.created_date }</p>
+        { this.props.byline ? <h2> {this.props.byline} </h2> : null }
+        { this.props.image ? <img src={this.props.image} href="#" alt="" /> : null }
+        <p> { this.props.abstract } </p>
       </div>
     )
   }
 }
 
 export default Article;
+
